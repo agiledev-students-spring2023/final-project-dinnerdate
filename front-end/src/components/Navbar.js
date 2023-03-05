c1c039656802a4c707144f96dfe06fea7fcffae6
wengf2086 <wengf2086@gmail.com>
2023-03-05 00:09:52 -0500

    created pages folder and home.js

---
+++ front-end/src/components/Navbar.js
@@ -0,0 +1,12 @@
+const NavBar = () => {
+    return (
+        <nav className="navbar">
+            <h1>DinnerDate</h1>
+            <a href="/messages">Messages</a>
+            <a href="/">Home</a>
+            <a href="/profile">My Profile</a>
+        </nav>
+    );
+}
+
+export default NavBar;
