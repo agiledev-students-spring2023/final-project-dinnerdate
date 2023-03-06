485e7a166ed0f2bd00053c434c57f831137f8343
victoriacarroll323 <vc2129@nyu.edu>
2023-03-06 13:04:42 -0500

    Completed profile page

--- front-end/src/pages/profile.js
+++ front-end/src/pages/profile.js
@@ -1,24 +1,34 @@
 
-
 const Profile = () => {
     return (
         <div className="profile">
             <div class="container">
-            <div class="box">
-                <img style={{width:"160px", height:"160px", borderRadius:"80px"}}
+            <div class="box" style={{paddingBottom:"30px"}}>
+                <img style={{width:"200px", height:"200px", borderRadius:"80px", paddingBottom:"30px"}}
                 src="https://www.nicepng.com/png/detail/933-9332131_profile-picture-default-png.png" alt="profile image"/> 
+                <button>edit</button>
             </div>
             <div class="About">
-            <ul>
-            <li><h4> Name: </h4></li>   
-            <li> User's first name, last name</li>
-            <li><h4>Email address: </h4></li>
-            <li> User's email address</li>
-           <li><h4>Phone number:</h4></li>
-            <li><h4> User's phone number</h4></li>
-           <li><h4> Date of birth:</h4></li>
-            <li> User's DOB </li>
-            </ul>
+            <div style={{paddingBottom:"10px"}}>
+                <h3 style={{paddingBottom:"5px"}}><button>edit</button> Name: </h3>  
+                    <p> User's first name, last name</p>
+            </div>
+            <div style={{paddingBottom:"10px"}}>
+                <h3 style={{paddingBottom:"5px"}}><button>edit</button> Email address: </h3>
+                 <p> User's email address</p>
+            </div>
+            <div style={{paddingBottom:"10px"}}>
+                <h3 style={{paddingBottom:"5px"}}><button>edit</button>Phone number:</h3>
+                    <p> User's phone number</p>
+            </div>
+            <div style={{paddingBottom:"10px"}} >
+                <h3 style={{paddingBottom:"5px"}}><button>edit</button> Date of birth:</h3>
+                    <p> User's DOB </p>
+            </div>
+            <div style={{paddingBottom:"10px"}}>
+            <h3 style={{paddingBottom:"5px"}}><button>edit</button> Password:</h3>
+                    <p> ********* </p>
+            </div>
             </div>
         </div>
         </div>
