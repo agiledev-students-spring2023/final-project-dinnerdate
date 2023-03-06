8c4de31c292efd53cd4dc4a1fcf41513ec1dae28
victoriacarroll323 <vc2129@nyu.edu>
2023-03-06 12:09:35 -0500

    Rough layout of Profile Page

--- front-end/src/pages/profile.js
+++ front-end/src/pages/profile.js
@@ -1,7 +1,26 @@
+
+
 const Profile = () => {
     return (
         <div className="profile">
-            <h2>This is your profile.</h2>
+            <div class="container">
+            <div class="box">
+                <img style={{width:"160px", height:"160px", borderRadius:"80px"}}
+                src="https://www.nicepng.com/png/detail/933-9332131_profile-picture-default-png.png" alt="profile image"/> 
+            </div>
+            <div class="About">
+            <ul>
+            <li><h4> Name: </h4></li>   
+            <li> User's first name, last name</li>
+            <li><h4>Email address: </h4></li>
+            <li> User's email address</li>
+           <li><h4>Phone number:</h4></li>
+            <li><h4> User's phone number</h4></li>
+           <li><h4> Date of birth:</h4></li>
+            <li> User's DOB </li>
+            </ul>
+            </div>
+        </div>
         </div>
     );
 }
