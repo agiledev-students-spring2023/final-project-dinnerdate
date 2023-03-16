9f0d518743288606b0e06e9d08bc605a7297981b
Frank Weng <78763842+wengf2086@users.noreply.github.com>
2023-03-15 20:15:00 -0400

    Created home page, location info page, and began implementation for create-post page

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,10 +1,11 @@
 import Navbar from './components/Navbar'
-import Home from './pages/home'
 import Messages from './pages/messages'
 import Profile from './pages/profile'
 import EditPost from './pages/editpost'
 import Login from './pages/login'
-
+import LocationInfo from './pages/location-info'
+import CreatePost from './pages/createpost'
+import Home from './pages/home'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
@@ -18,6 +19,8 @@ function App() {
             <Route path="/profile"><Profile /></Route>
             <Route path="/edit-post"><EditPost /></Route>
             <Route path="/login"><Login /></Route>
+            <Route path="/location-info"><LocationInfo /></Route>
+            <Route path="/create-post"><CreatePost /></Route>
             <Route path="/"><Home /></Route>
           </Switch>
         </div>
---
+++ front-end/src/pages/createpost.js
@@ -0,0 +1,9 @@
+const CreatePost = () => {
+    return (
+        <div className="create-post">
+            <h1>Create a Post</h1>
+        </div>
+    );
+}
+
+export default CreatePost;
