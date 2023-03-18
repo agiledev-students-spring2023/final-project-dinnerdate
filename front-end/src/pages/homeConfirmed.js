c7b7f422656c11fd52574feffced28fdd81503b8
clairekguo <claireg3819@gmail.com>
2023-03-18 19:03:43 -0400

    create date confirmed home page

--- front-end/src/pages/homeConfirmed.js
+++ front-end/src/pages/homeConfirmed.js
@@ -1,8 +1,16 @@
 import './homeConfirmed.css';
+import Button from '../components/Button.js'
+
 const HomeConfirmed = () => {
     return (
-        <div className="home">
-            <h1>CONFIRMED</h1>
+        <div className="homeConfirmed">
+            <h1>Date with [DINER NAME]</h1>
+            <h3>You have a date with [Diner Name] at [Restaurant Name] on MM/DD/YYYY at HH:MM. Don’t be late!</h3>
+
+            <div><Button text="Chat with [DINER NAME]"/></div>
+            <div><Button text="Get Directions"/></div>
+            <div><Button text="Edit Date"/></div>
+            <div><Button text="Cancel Date"/></div>
         </div>
     );
 }
