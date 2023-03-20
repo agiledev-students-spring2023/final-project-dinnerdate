d083d827a5854cbeeb12964f2624856c0047d6c9
clairekguo <claireg3819@gmail.com>
2023-03-20 12:26:09 -0400

    Merge remote-tracking branch 'origin/master' into user-story/17/task/20/user-login-registration

--- front-end/src/pages/profile.js
+++ front-end/src/pages/profile.js
@@ -1,45 +1,43 @@
-
+import './profile.css'
+import Button from '../components/Button.js'
 const Profile = () => {
     return (
         <div className="profile">
-            <div class="container">
-            <div class="box" style={{paddingBottom:"30px"}}>
-                <img style={{width:"200px", height:"200px", borderRadius:"80px", paddingBottom:"30px"}}
-                src="https://www.nicepng.com/png/detail/933-9332131_profile-picture-default-png.png" alt=""/> 
-                <button>edit</button>
+            <div class="box" style={{ paddingBottom: "30px" }}>
+                    <img style={{ width: "200px", height: "200px", borderRadius: "80px", paddingBottom: "30px" }}
+                        src="https://www.nicepng.com/png/detail/933-9332131_profile-picture-default-png.png" alt="" />
+                    <Button text="Edit"/>
             </div>
-            <div class="About">
-            <div style={{paddingBottom:"10px"}}>
-                <h3 style={{paddingBottom:"5px"}}><button>edit</button> Name: </h3>  
-                    <p> User's first name, last name</p>
-            </div>
-            <div style={{paddingBottom:"10px"}}>
-                <h3 style={{paddingBottom:"5px"}}><button>edit</button> Bio:</h3>
-                <p> Description user will write about themselves for other viewers to see</p>
-            </div>
-            <div style={{paddingBottom:"10px"}}>
-                <h3 style={{paddingBottom:"5px"}}><button>edit</button> Email address: </h3>
-                 <p> User's email address</p>
-            </div>
-            <div style={{paddingBottom:"10px"}}>
-                <h3 style={{paddingBottom:"5px"}}><button>edit</button> Phone number:</h3>
-                    <p> User's phone number</p>
-            </div>
-            <div style={{paddingBottom:"10px"}} >
-                <h3 style={{paddingBottom:"5px"}}><button>edit</button> Date of birth:</h3>
-                    <p> User's DOB </p>
-            </div>
-            <div style={{paddingBottom:"10px"}}>
-            <h3 style={{paddingBottom:"5px"}}><button>edit</button> Password:</h3>
-                    <p> ********* </p>
-            </div>
+            <div className='editProfile'>
+                <form >
+                    <label>
+                    <p>Name</p>
+                    <input type="text" placeholder="User Name"/>
+                    </label>
+                    <label>
+                    <p>Email</p>
+                    <input type="text" placeholder="email@email.com"/>
+                    </label>
+                    <label>
+                    <p>Phone</p>
+                    <input type="text" placeholder="000-000-0000"/>
+                    </label>
+                    <label>
+                    <p>DOB</p>
+                    <input type="password" placeholder="MM/DD/YYYY"/>
+                    </label>
+                    <label>
+                    <p>Password</p>
+                    <input type="password" placeholder="********"/>
+                    </label>
+                    <div className="save-btn"><Button text="Save"/></div>
+                </form>
             </div>
             <div style={{padding:"30px"}}>
                 <h3>Your last post:</h3>
                 <p>User's last post they uploaded for date</p>
                 <p style={{fontSize:"10px"}}>Date and time post was created</p>
             </div>
-        </div>
         </div>
     );
 }
