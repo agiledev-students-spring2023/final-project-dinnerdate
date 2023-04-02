10ad06f5a4a0e7e8c2fc0303c32949b48545a075
Frank Weng <78763842+wengf2086@users.noreply.github.com>
2023-04-02 16:47:22 -0400

    Refactored App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,20 +1,21 @@
+
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { LocalizationProvider } from '@mui/x-date-pickers';
 import Navbar from './components/Navbar'
+
 import Chat from './pages/chat'
 import Conversation from './pages/conversation'
-import Profile from './pages/profile'
-import OtherProfile from './pages/otherprofile'
-import EditPost from './pages/editpost'
-import Login from './pages/login'
-import Register from './pages/register'
-import LocationInfo from './pages/location-info'
 import CreatePost from './pages/createpost'
+import EditPost from './pages/editpost'
 import Home from './pages/home'
 import HomeConfirmed from './pages/homeConfirmed'
 import HomeLFD from './pages/home-lfd'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
-
+import LocationInfo from './pages/location-info'
+import Login from './pages/login'
+import OtherProfile from './pages/otherprofile'
+import Profile from './pages/profile'
+import Register from './pages/register'
 
 function App() {
   return (
@@ -27,13 +28,17 @@ function App() {
             <Switch>
               <Route path="/chat"><Chat /></Route>
               <Route path="/conversation"><Conversation/></Route>
-              <Route path="/profile"><Profile /></Route>
-              <Route path="/user"><OtherProfile /></Route>
-              <Route path="/edit-post"><EditPost /></Route>
-              <Route path="/login"><Login /></Route>
-              <Route path="/register"><Register /></Route>
-              <Route path="/location-info"><LocationInfo /></Route>
+
               <Route path="/create-post"><CreatePost /></Route>
+              <Route path="/edit-post"><EditPost /></Route>
+              
+              <Route path="/user"><OtherProfile /></Route>
+              <Route path="/profile"><Profile /></Route>
+
+              <Route path="/login"><Login /></Route>           
+              <Route path="/register"><Register /></Route>
+
+              <Route path="/location-info"><LocationInfo /></Route>
               <Route path="/date"><HomeConfirmed /></Route>
               <Route path="/home-lfd"><HomeLFD /></Route>
               <Route path="/"><Home /></Route>
