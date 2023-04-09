4c99a4413c6fc158a0d74d04db87166809c3fa34
clairekguo <claireg3819@gmail.com>
2023-04-08 22:07:33 -0400

    profile pic stockimage deadlink -- updated

--- front-end/src/pages/restaurant-info.js
+++ front-end/src/pages/restaurant-info.js
@@ -149,7 +149,7 @@ const RestaurantInfo = ( props ) => {
                             <h1>[Diner Name], [Age]</h1>
                             <div id="wrapper">
                                 <div id="first"><img style={{ width: "200px", height: "200px", borderRadius: "80px", paddingBottom: "30px" }}
-                                        src="https://www.nicepng.com/png/detail/933-9332131_profile-picture-default-png.png" alt="" /></div>
+                                        src="https://www.freeiconspng.com/thumbs/profile-icon-png/am-a-19-year-old-multimedia-artist-student-from-manila--21.png" alt="" /></div>
                                 <div id="second">The User bio will appear here...</div>
                             </div>
                             <div className="dec-btn"><div onClick={() => setButtonPopup(false)}><button>Decline</button></div></div>
