6b654fc12b97d875d00706be2f101c31418277ae
victoriacarroll323 <vc2129@nyu.edu>
2023-04-24 11:58:41 -0400

    change navbar to say profile if logged in

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -1,13 +1,20 @@
 import './navbar.css';
 import { Link } from "react-router-dom";
-const Navbar = () => {
+const Navbar = ({ isLoggedIn, handleLogout}) => {
     return (
         <nav className="navbar">
             <Link to='/'><h1>DinnerDate</h1></Link>
             <div className="links">
                 <Link to='/inbox'>Inbox</Link>
                 <Link to='/login'>Login</Link>
-                {/* <Link to='/profile'>My Profile</Link> */}
+                {isLoggedIn ? (
+                    <>
+                        <Link to='/profile'>Profile</Link>
+                        <button onClick = {handleLogout}>Logout</button>
+                            </>
+                ) : (
+                    <Link to='/login'>Login</Link>
+                )}
             </div>
         </nav>
     );
