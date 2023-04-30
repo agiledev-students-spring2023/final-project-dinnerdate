8ab9d4b4a6d6ece2f81b33d89b4e4bd641710c30
wengf2086 <wengf2086@gmail.com>
2023-04-30 17:43:53 -0400

    Added home and requests to navbar

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -5,6 +5,8 @@ const Navbar = () => {
         <nav className="navbar">
             <Link to='/'><h1>DinnerDate</h1></Link>
             <div className="links">
+                <Link to='/'>Home</Link>
+                <Link to='/requests'>My Requests</Link>
                 <Link to='/inbox'>Inbox</Link>
                 <Link to='/profile'>Profile</Link>
                 {/* <Link to='/profile'>My Profile</Link> */}
