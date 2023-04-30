b6b5f438507f701af78006bb9fc3227e2f0dd3d2
wengf2086 <wengf2086@gmail.com>
2023-04-30 17:56:47 -0400

    Display username instead of Profile in navbar

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -1,15 +1,23 @@
 import './navbar.css';
+import { useState, useEffect } from 'react';
+import axios from '../axiosInstance';
 import { Link } from "react-router-dom";
 const Navbar = () => {
+    const [user, setUser] = useState();
+    const userId = (localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')).id : '');
+    useEffect(() => {
+        axios.get(`/user/${userId}`)
+        .then(res => setUser(res.data))
+        .catch(e => console.error(e.response.data.message));
+      }, [])
+
     return (
         <nav className="navbar">
             <Link to='/'><h1>DinnerDate</h1></Link>
             <div className="links">
                 <Link to='/'>Home</Link>
-                <Link to='/requests'>My Requests</Link>
                 <Link to='/inbox'>Inbox</Link>
-                <Link to='/profile'>Profile</Link>
-                {/* <Link to='/profile'>My Profile</Link> */}
+                <Link to='/profile'>{user?.firstName + " " + user?.lastName}</Link>
             </div>
         </nav>
     );
