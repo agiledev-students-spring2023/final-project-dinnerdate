65af60d6fa1f60eaf9a24dd014a6c8ad6d519164
wengf2086 <wengf2086@gmail.com>
2023-05-01 17:18:23 -0400

    Fixed username display in NavBar

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -3,14 +3,11 @@ import { useState, useEffect } from 'react';
 import axios from '../axiosInstance';
 import { Link } from "react-router-dom";
 const Navbar = () => {
-    const [user, setUser] = useState();
-    const userId = (localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')).id : '');
+    const [user, setUser] = useState()
     useEffect(() => {
-        axios.get(`/user/${userId}`)
-        .then(res => setUser(res.data))
-        .catch(e => console.error(e.response.data.message));
-      }, [])
-
+        setUser((localStorage.getItem('user') ? JSON.parse(localStorage.getItem('user')) : null));
+    }, [])
+    
     return (
         <nav className="navbar">
             <Link to='/'><h1>DinnerDate</h1></Link>
